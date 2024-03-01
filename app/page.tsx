9a85fee1f8075f09a23e70eba068ea31fa37cff7
Orlandini Sebastien <sebastien.orlandini@gmail.com>
2024-03-01 17:47:29 +0100

    mobile map

--- app/page.tsx
+++ app/page.tsx
@@ -62,7 +62,7 @@ export default function Home(): JSX.Element {
 			</div>
 
 			<div className="w-11/12 flex xl:hidden max-xl:flex-col my-4">
-				<div className="text-gray-900 md:text-lg lg:text-xl  xl:text-2xl font-bold h-2/3 px-4 flex justify-start items-center w-full   ">
+				<div className="text-gray-900 md:text-lg lg:text-xl  xl:text-2xl font-bold h-2/3 px-4 flex md:justify-start max-md:justify-center items-center w-full   ">
 					<Image
 						className="rounded-full border-2 border-orange-500 shadow-xl mr-4"
 						width={100}
@@ -92,7 +92,7 @@ export default function Home(): JSX.Element {
 						src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d46193.85233993836!2d6.146763710673335!3d49.10508885509453!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4794dc1b6074b6a9%3A0x596be4b635bba669!2sMetz!5e0!3m2!1sfr!2sfr!4v1709292302569!5m2!1sfr!2sfr"
 						width="300"
 						height="300"
-						className="border-2 rounded-full border-orange-500 shadow-xl "
+						className="border-2 md:rounded-full border-orange-500 shadow-xl "
 						loading="lazy"
 					></iframe>
 				</div>
