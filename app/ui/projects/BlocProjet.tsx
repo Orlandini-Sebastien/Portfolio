6862e6d376030b64ecbaba9b6c633aec08b65c40
Orlandini Sebastien <sebastien.orlandini@gmail.com>
2024-03-07 21:53:23 +0100

    resize

--- app/ui/projects/BlocProjet.tsx
+++ app/ui/projects/BlocProjet.tsx
@@ -1,63 +1,24 @@
-'use client'
+
 import { ReactElement } from 'react'
 import TitleProjet from './TitleProjet'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
-import { useState, useEffect } from 'react'
 import data from '../../../public/dataProjet.json'
 import Link from 'next/link'
 import Image from 'next/image'
 
-function useWindowSize() {
-  const [windowSize, setWindowSize] = useState({
-    width: window.innerWidth,
-    height: window.innerHeight,
-  })
-
-  useEffect(() => {
-    function handleResize() {
-      setWindowSize({
-        width: window.innerWidth,
-        height: window.innerHeight,
-      })
-    }
-
-    window.addEventListener('resize', handleResize)
-    return () => window.removeEventListener('resize', handleResize)
-  }, [])
-
-  return windowSize
-}
-
 const BlocProgress = (): ReactElement => {
-  const { width: windowWidth } = useWindowSize()
-  let width: number, height: number
-  let widthIcon : number, heightIcon:number
-
-  // Définir les dimensions en fonction de la largeur de la fenêtre
-  if (windowWidth < 640) {
-    width = 500
-    height = 500
-  } else if (windowWidth >= 640 && windowWidth < 1200) {
-    width = 400
-    height = 400
-  } else {
-    width = 500
-    height = 500
-   
-  }
-
   return (
     <section>
       {data.map((item, index) => {
         return (
           <div key={index}>
             <TitleProjet title={item.projet} />
-            <div className="max-lg:flex-col  my-2 flex w-full items-center rounded-lg border border-b-2 bg-slate-50 p-2 shadow  ">
+            <div className="my-2  flex w-full items-center rounded-lg border border-b-2 bg-slate-50 p-2 shadow max-lg:flex-col  ">
               <Image
                 loading="lazy"
-                width={width}
-                height={height}
+                width={500}
+                height={500}
                 src={item.image}
                 alt={item.projet}
               />
@@ -97,8 +58,8 @@ const BlocProgress = (): ReactElement => {
                       <Image
                         key={index}
                         className="mx-6 my-2 shrink-0  "
-                        width={width/15}
-                        height={height/15}
+                        width={50}
+                        height={50}
                         src={techno.icon}
                         alt={techno.name}
                       />
