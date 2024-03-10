f62a7f6325f15957f3939541c578221929083ca5
Orlandini Sebastien <sebastien.orlandini@gmail.com>
2024-03-10 20:30:55 +0100

    en quete

--- app/page.tsx
+++ app/page.tsx
@@ -78,7 +78,7 @@ export default function Home(): JSX.Element {
               <span className={`${styles.cursor} text-red-300`}> {name}</span>
             </p>{' '}
             <div className="md:text-md text-gray-600 lg:text-lg xl:text-xl  ">
-              En recherche d&apos;opportunité
+              En quête de nouveaux défis professionnels.
             </div>
           </div>
         </div>
